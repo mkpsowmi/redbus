73395bd7d2ee90b2b1f59750e787b63c706d922b
sowmiyamkp <122273869+sowmiyamkp@users.noreply.github.com>
2024-12-16 18:02:47 +0530

    commit

--- src/Componend/RedBus/Routing.js
+++ src/Componend/RedBus/Routing.js
@@ -13,7 +13,7 @@ const Routing = () => {
   return (
     <div>
     <Context.Provider value={{state,dispatch}}> 
-        <BrowserRouter>
+        <BrowserRouter basename='/redbus'>
           <Routes>
             <Route path="/"  element={<Home/>}></Route>
             <Route path="/Travels" element={<Travels/>}/>
